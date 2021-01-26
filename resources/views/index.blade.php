f69f848f9b0369e2776edd24354768cef0621fc6
Lizbeth <lizbaez.140799@gmail.com>
2021-01-26 14:06:05 -0700

    Videoconferencia2

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi sistema</title>
+</head>
+<body>
+    <h1>Estás en la pantalla de inicio</h1>
+
+    <p>Este es un párrafo HTML</p>
+</body>
+</html>
